89cecc5c00ed0d8545627c7562d904b4d4aa09bb
Vadász Péter <pevad95@gmail.com>
2019-03-26 22:41:09 +0100

    Add comments.

--- copy_ctor.cpp
+++ copy_ctor.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include "Student.h"
 
+Student doSth(Student student) { //copy ctor
+	return student; //copy ctor mert érték szerint adunk vissza egy objektumot
+}
+
 int main() {
 	Student s("Darth Vader", "AAAAAA");
 	s.addMark(2);
 	s.addMark(5);
-	Student s2 = s;
-	Student s3(s);
+	Student s2 = s; //copy ctor
+	Student s3(s); //copy ctor
 	
 	std::cout << s.printMarks() << std::endl;
 	std::cout << s2.printMarks() << std::endl;
@@ -14,6 +18,7 @@ int main() {
 	
 	std::cout << (s == s2) << std::endl;
 	std::cout << s << std::endl;
+	doSth(s);
 	
 	return 0;
 }
